d491aee03f54dcd0b8340cc8307b1e7784a0e6b5
Sirapat Boonyasiwapong <aun.sirapat@gmail.com>
2017-10-30 17:22:43 +0700

    no message

--- Sources/App/Controllers/ClassesController.swift
+++ Sources/App/Controllers/ClassesController.swift
@@ -24,18 +24,25 @@ public final class ClassesController {
     }
     
     //POST Create class
-    func classForm(request: Request) throws ->  ResponseRepresentable {
+    func classForm(request: Request) throws -> ResponseRepresentable {
         guard let name = request.data["name"]?.string,
         let events = request.data["events"]?.string,
-        let users = request.data["users"]?.string else {
-            throw Abort.badRequest
+        let users = request.data["users"]?.string
+            else{
+                throw Abort.badRequest
         }
-        
         let classes = Class(name: name, events: events, users: users)
         try classes.save()
         
-        return Response(redirect: "/classes")
+        return Response(redirect: "/")
     }
+    
+    //GET Edit class
+    func editClassForm(request: Request) throws -> ResponseRepresentable {
+        
+        return try render("Classes/class-edit", [:], for: request, with: view)
+    }
+    
     
 }
 
